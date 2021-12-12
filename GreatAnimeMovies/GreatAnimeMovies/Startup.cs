cde0d202b1fe489dc2df4bd34b5b3078d1a42e02
chloebaker <62111646+chloebaker@users.noreply.github.com>
2021-12-12 15:36:01 -0600

    editted startup.cs so SQLite works

--- GreatAnimeMovies/GreatAnimeMovies/Startup.cs
+++ GreatAnimeMovies/GreatAnimeMovies/Startup.cs
@@ -15,20 +15,32 @@ namespace GreatAnimeMovies
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
         {
+            Environment = env;
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
+        public IWebHostEnvironment Environment { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+            if (Environment.IsDevelopment())
+            {
+                services.AddDbContext<GreatAnimeMoviesContext>(options =>
+                options.UseSqlite(
+                    Configuration.GetConnectionString("GreatAnimeMoviesContext")));
+            }
+            else
+            {
+                services.AddDbContext<GreatAnimeMoviesContext>(options =>
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("MoviesContext")));
+            }
 
-            services.AddDbContext<GreatAnimeMoviesContext>(options =>
-                    options.UseSqlite(Configuration.GetConnectionString("GreatAnimeMoviesContext")));
+            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
