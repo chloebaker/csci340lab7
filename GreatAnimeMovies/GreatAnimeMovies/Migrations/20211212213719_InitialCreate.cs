413684ceb4d193c60b2eabed98c3633a9c41adbf
chloebaker <62111646+chloebaker@users.noreply.github.com>
2021-12-12 15:38:01 -0600

    migrated

---
+++ GreatAnimeMovies/GreatAnimeMovies/Migrations/20211212213719_InitialCreate.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GreatAnimeMovies.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Movie",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(type: "TEXT", nullable: true),
+                    ReleaseDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    ProductionStudio = table.Column<string>(type: "TEXT", nullable: true),
+                    Director = table.Column<string>(type: "TEXT", nullable: true),
+                    Genre = table.Column<int>(type: "INTEGER", nullable: false),
+                    EnglishDub = table.Column<bool>(type: "INTEGER", nullable: false),
+                    MPAARating = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movie", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Movie");
+        }
+    }
+}
